b3a380e1ffcb4edb83fa293e65d687323ed95858
BukkitGerman <tanks110011@gmail.com>
2020-09-22 13:23:36 +0200

    Changes to be committed: 	modified:   Scripts/Ruby/JsonToCSV.rb 	new file:   Scripts/stats.csv
    Change the JsonToCSV.rb Script, adjust the Time value.

--- Scripts/Ruby/JsonToCSV.rb
+++ Scripts/Ruby/JsonToCSV.rb
@@ -7,7 +7,9 @@ require 'csv'
 
 uri = "https://status.gamer4life.net/status.php"
 result = JSON.parse(Net::HTTP.get(URI.parse(uri)))
+time = Time.now
+timestamp = time.month.to_s + ":" + time.day.to_s << ":" + time.hour.to_s
 
 CSV.open("stats.csv", "a+") do |csv|
-		csv << [Time.now, result["status"]]
+		csv << [timestamp, result["status"]]
 end
