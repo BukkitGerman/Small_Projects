81e00cb272c5c46627a8d90b5ca102318cc2ad53
BukkitGerman <tanks110011@gmail.com>
2020-09-24 14:00:42 +0200

    Update JsonToCSV Script 	- Add the numberofusers.csv

--- Scripts/Ruby/JsonToCSV.rb
+++ Scripts/Ruby/JsonToCSV.rb
@@ -7,9 +7,14 @@ require 'csv'
 
 uri = "https://status.gamer4life.net/status.php"
 result = JSON.parse(Net::HTTP.get(URI.parse(uri)))
+
 time = Time.now
 timestamp = time.month.to_s + ":" + time.day.to_s << ":" + time.hour.to_s
 
 CSV.open("uptime.csv", "a+") do |csv|
 		csv << [timestamp, result["status"]]
+end
+
+CSV.open("numberofusers.csv", "a+") do |csv|
+	csv << [timestamp, result["user"]]
 end
